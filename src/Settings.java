1f6a1c0291e6c319bf1bacaf551d51ff8fc0ce98
Arkadiusz Żukiewicz <arkadiusz.zukiewicz@gmail.com>
2019-10-04 23:39:12 +0200

    feat: database, settings
    Initial database connection and settings class;
    
    You have to create on local database HotelReservation user HotelReservation with pass HotelReservation
    in future I'd like to manage it through settings wizard.

---
+++ src/Settings.java
@@ -0,0 +1,19 @@
+
+public class Settings
+{
+	public String getDBHost() {
+		return "localhost";
+	}
+
+	public String getDBName() {
+		return "HotelReservation";
+	}
+
+	public String getDBUser() {
+		return "HotelReservation";
+	}
+
+	public String getDBPassword() {
+		return "HotelReservation";
+	}
+}
