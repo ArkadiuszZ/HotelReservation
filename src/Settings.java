256bbf836e69b9020e29046448ec07e16c927550
Arkadiusz Żukiewicz <arkadiusz.zukiewicz@gmail.com>
2019-10-05 09:38:59 +0200

    updated Settings class to support file

--- src/Settings.java
+++ src/Settings.java
@@ -1,19 +1,47 @@
+import java.util.prefs.Preferences;
 
 public class Settings
 {
-	public String getDBHost() {
-		return "localhost";
+
+	private Preferences preferenceFile = Preferences.userRoot().node("HotelReservation.properties");
+
+	public String getDBHost()
+	{
+		return preferenceFile.get("DBHost", "localhost");
 	}
 
-	public String getDBName() {
-		return "HotelReservation";
+	public String getDBName()
+	{
+		return  preferenceFile.get("DBName", "HotelReservation");
 	}
 
-	public String getDBUser() {
-		return "HotelReservation";
+	public String getDBUser()
+	{
+		return  preferenceFile.get("DBUser", "HotelReservation");
 	}
 
-	public String getDBPassword() {
-		return "HotelReservation";
+	public String getDBPassword()
+	{
+		return  preferenceFile.get("DBPassword", "HotelReservation");
+	}
+
+	public void setDBHost(String host)
+	{
+		preferenceFile.put("DBHost", host);
+	}
+
+	public void setDBName(String name)
+	{
+		preference.put("DBName", name);
+	}
+
+	public void setDBUser(String user)
+	{
+		preference.put("DBUser", user);
+	}
+
+	public void setDBPassword(String password)
+	{
+		preference.put("DBPassword", password);
 	}
 }
